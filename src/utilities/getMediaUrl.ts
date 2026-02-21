26de8fff883c6260cbf5d02f0c2ba2e5c87d22e0
Recep GÜÇLÜER <rgucluer@gmail.com>
2026-02-21 12:53:18 +0300

    fix: Selective testing of strings

--- src/utilities/getMediaUrl.ts
+++ src/utilities/getMediaUrl.ts
@@ -9,9 +9,9 @@ import { getClientSideURL } from '@/utilities/getURL'
 export const getMediaUrl = (url: string | null | undefined, cacheTag?: string | null): string => {
   if (!url) return ''
 
-  if (cacheTag && cacheTag !== '') {
-    cacheTag = encodeURIComponent(cacheTag)
-  }
+  // if (cacheTag && cacheTag !== '') {
+  //   cacheTag = encodeURIComponent(cacheTag)
+  // }
 
   // // Check if URL already has http/https protocol
   // if (url.startsWith('http://') || url.startsWith('https://')) {
@@ -20,5 +20,6 @@ export const getMediaUrl = (url: string | null | undefined, cacheTag?: string | 
 
   // Otherwise prepend client-side URL
   const baseUrl = getClientSideURL()
-  return cacheTag ? `${baseUrl}${url}?${cacheTag}` : `${baseUrl}${url}`
+  // return cacheTag ? `${baseUrl}${url}?${cacheTag}` : `${baseUrl}${url}`
+  return baseUrl
 }
